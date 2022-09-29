317237e1f7b372c92eec88cf1d8d4a59b51db6cc
mglouman <moadgoulman@gmail.com>
2022-09-29 04:10:31 +0100

    fixing: testimonial is flipping css bugs

--- src/components/testimonials/Testimonials.js
+++ src/components/testimonials/Testimonials.js
@@ -1,10 +1,67 @@
-import React from 'react'
-import "./testimonials.css"
+import React from "react";
+import "./testimonials.css";
+import AVTR1 from "../../assets/avatar1.jpg";
+import AVTR2 from "../../assets/avatar2.jpg";
+import AVTR3 from "../../assets/avatar3.jpg";
+import AVTR4 from "../../assets/avatar4.jpg";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const data = [
+  {
+    avatar: AVTR1,
+    name: "Client Name",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, enim.",
+  },
+  {
+    avatar: AVTR2,
+    name: "Client Name",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, enim.",
+  },
+  {
+    avatar: AVTR3,
+    name: "Client Name",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, enim.",
+  },
+  {
+    avatar: AVTR4,
+    name: "Client Name",
+    review:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, enim.",
+  },
+];
 
 const Testimonials = () => {
   return (
-    <div>Testimonials</div>
-  )
-}
+    <section id="testimonials">
+      <h5>Review From Clients</h5>
+      <h2>Testimonials</h2>
+      <Swiper
+        pagination={{
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="container testimonials_container"
+      >
+        {data.map(({ avatar, name, review }, index) => {
+          return (
+            <SwiperSlide key={index} className="testimonial">
+              <div className="client_avatar">
+                <img src={avatar} alt={name} />
+              </div>
+              <h5 className="client_name">{name}</h5>
+              <small className="client_review">{review}</small>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
