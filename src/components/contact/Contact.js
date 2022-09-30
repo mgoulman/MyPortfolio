10ea3770d64f4386748d098666a8d8ae21e11691
mglouman <moadgoulman@gmail.com>
2022-10-01 00:01:42 +0100

    finishin the contact and footer

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,89 @@
-import React from 'react'
-import "./contact.css"
+import React, { useRef } from "react";
+import "./contact.css";
+import { MdOutlineEmail } from "react-icons/md";
+import { FaFacebookMessenger, FaWhatsapp } from "react-icons/fa";
+import emailjs from "emailjs-com";
 
 const Contact = () => {
-  return (
-    <section id="contact">Contact</section>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_9c55b2m",
+        "template_v6puilo",
+        form.current,
+        "g9cUgUraQDv3AWABP"
+      )
+      e.target.reset();
+  };
+
+  return (
+    <section id="contact">
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact_container">
+        <div className="contact_options">
+          <article className="contact_option">
+            <MdOutlineEmail className=".contact_option-icon" />
+            <h4>Email</h4>
+            <h5>mouadgoulman@gmail.com</h5>
+            <a
+              href="mailto:moadgoulman@gmail.com"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              Send Me An Email
+            </a>
+          </article>
+          <article className="contact_option">
+            <FaFacebookMessenger className=".contact_option-icon" />
+            <h4>Messenger</h4>
+            <h5>Mouad Goulmane</h5>
+            <a
+              href="https://m.me/moad.goulman"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              Send Me A Message
+            </a>
+          </article>
+          <article className="contact_option">
+            <FaWhatsapp className=".contact_option-icon" />
+            <h4>WhatsApp</h4>
+            <h5>+212 674 194 490</h5>
+            <a
+              href="https://api.whatsapp.com/send?phone=21274194490"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              Send Me A Message
+            </a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input type="email" name="email" placeholder="Email" required />
+          <textarea
+            name="message"
+            rows="7"
+            placeholder="Your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send Message
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,56 @@
-import React from 'react'
-import "./footer.css"
+import React from "react";
+import "./footer.css";
+import { FaFacebookSquare } from "react-icons/fa";
+import { BsTwitter, BsInstagram, BsLinkedin } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer>
+      <a href="#header" className="footer_logo">
+        Mouad
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#header">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li>
+          <a href="#experience">Experience</a>
+        </li>
+        <li>
+          <a href="#services">Services</a>
+        </li>
+        <li>
+          <a href="#portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#testimonials">Testimonials</a>
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+      <div className="footer_socials">
+        <a href="https://www.linkedin.com/in/mouad-goulmane/">
+          <BsLinkedin />
+        </a>
+        <a href="https://www.facebook.com/moad.goulman/">
+          <FaFacebookSquare />
+        </a>
+        <a href="https://twitter.com/GoulmaneMouad">
+          <BsTwitter />
+        </a>
+        <a href="https://www.instagram.com/mouad_goulmane/">
+          <BsInstagram />
+        </a>
+      </div>
+      <div className="footer_copyright">
+        <small>&copy; Mouad Goulmane Portfolio.</small>
+      </div>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
